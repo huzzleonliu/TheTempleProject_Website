6358885394f29e417112db1d45d3a5f17d61c083
Huzz <huzzleonliu@foxmail.com>
2025-03-25 22:25:03 +0100

    2025-03-25 22:25:03

--- src/components/preview.rs
+++ src/components/preview.rs
@@ -4,9 +4,9 @@ use leptos::prelude::*;
 pub fn Preview() -> impl IntoView {
     view! {
         <div class="text-2xl">
-        <img src="https://the-temple-project-1321767328.cos.eu-frankfurt.myqcloud.com/2.jpg?q-sign-algorithm=sha1&q-ak=AKID_dnuos8ll-YRRpK2n6AFOloNraFDajq3MjSKuJoYhmOzluLFSgkbEzOYm7ITOesr&q-sign-time=1742846032;1742849632&q-key-time=1742846032;1742849632&q-header-list=host&q-url-param-list=ci-process&q-signature=75c686cd6b723af0c0bf8173a56b9a14c9c6e419&x-cos-security-token=7gp3rMwoBdHcjR9V7iBGdI47eJyJAC8a92232ec78254e78ba36114cb473881b8GLzD8X_I802rCLuNiHkoZKdrHzIB6aLAlk90jGPEdft5gzUY2nrR7jNkrNusm1p4JVsh2dh_a3XmCNlzVC1y7ggmdw9i_BjnUS4TyGYeSRKD7t2V7HO0xAne0iyl80MN3vWJYYWJ1FqX0P9LtcGoX836k_9PqvZW7oIKYJp4y38i0YWJ95xjBGHEnD2Nr9Tb&ci-process=originImage" class="box-border size-32" alt="not found"/>
-
+        <img src="https://the-temple-project-1321767328.cos.eu-frankfurt.myqcloud.com/2.jpg" class="box-border size-32" alt="not found"/>
         <img src="https://the-temple-project-1321767328.cos.eu-frankfurt.myqcloud.com/3.jpg" class="box-border size-32" alt="not found"/>
+        <img src="public/temp_picture/2.jpg" class="box-border size-32" alt="not found"/>
         <p>"this is Preview"</p>
         <p>"this is Preview"</p>
         <p>"this is Preview"</p>
