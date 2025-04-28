e3f869666d8e6653cdc4e910a1d39dbd052f162e
huzz <huzzleonliu@foxmail.com>
2025-04-28 13:24:49 +0200

    2025-04-28 13:24:49

--- frontend/start-docker.sh
+++ frontend/start-docker.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-podman run -d --user 1000:1000
-
--p 3000:3000
-
--v .:/frontend
-
-rust-trunk-frontend \
+podman run -d --user 1000:1000 \
+  -p 3000:3000 \
+  -v .:/frontend \
+  frontend \
   /bin/sh -c "./docker-start.sh"
