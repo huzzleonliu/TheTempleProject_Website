6d70f3c9e0f405af7be3b625546089e14dd647d5
huzz <huzzleonliu@foxmail.com>
2025-05-02 01:36:21 +0200

    2025-05-02 01:36:21

--- frontend/docker-start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-trunk serve --release
--- frontend/start-docker.sh
+++ frontend/start-docker.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-podman run -d --user 1000:1000 \
+podman run -d \
   -p 3000:3000 \
   -v .:/frontend \
-  frontend \
-  /bin/sh -c "./docker-start.sh"
+  frontend
