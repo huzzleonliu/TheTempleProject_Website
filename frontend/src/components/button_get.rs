2935145a2740c722a043a1bc1cf265977510124f
Huzz <huzzleonliu@foxmail.com>
2025-05-08 13:33:33 +0200

    2025-05-08 13:33:33

--- frontend/src/components/button_get.rs
+++ frontend/src/components/button_get.rs
@@ -12,7 +12,7 @@ pub fn ButtonGet() -> impl IntoView {
             // 发送请求前显示加载状态
             set_response.set("Loading...".into());
             
-            match Request::get("http://backend:8081/print")
+            match Request::get("http://43.131.27.176/api/print")
                 .send()
                 .await 
             {
