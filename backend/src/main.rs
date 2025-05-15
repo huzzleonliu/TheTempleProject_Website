2d2b7be1d3fb02f6aa14f04d4b1cd8791b0004b4
Huzz <huzzleonliu@foxmail.com>
2025-05-15 10:55:28 +0200

    2025-05-15 10:55:28

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -15,7 +15,7 @@ async fn main() {
                 .allow_origin(Any)
                 .allow_methods(Any)
                 .allow_headers(Any)
-                .allow_credentials(true)
+                // .allow_credentials(true)
         );
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8081));
