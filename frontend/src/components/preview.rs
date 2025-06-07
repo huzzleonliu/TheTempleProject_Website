0ac75e605b4fa8d6a224fb56673f6ad8db718bb9
huzz <huzzleonliu@foxmail.com>
2025-06-07 22:30:42 +0200

    解决了图床和图片导入的问题

--- frontend/src/components/preview.rs
+++ frontend/src/components/preview.rs
@@ -5,8 +5,9 @@ pub fn Preview() -> impl IntoView {
     view! {
         // <div class="text-2xl">
         <div>
-        // <img src="https://the-temple-project-1321767328.cos.eu-frankfurt.myqcloud.com/2.jpg" class="box-border size-32" alt="not found"/>
-        // <img src="https://the-temple-project-1321767328.cos.eu-frankfurt.myqcloud.com/3.jpg" class="box-border size-32" alt="not found"/>
+            <img src="http://tp_resource:80/resource/3.jpg" class="box-border size-32" alt="http://tp_resource.:80/3.jpg"/>
+            <img src="http://43.131.27.176:8084/resource/3.jpg" class="box-border size-32" alt="http://43.131.27.176:8084/resource/3.jpg"/>
+            <img src="http://localhost:8084/resource/3.jpg" class="box-border size-32" alt="http://localhost:8084/resource/3.jpg"/>
             <img src="/E2.jpg" class="box-border size-32" alt="can not get"/>
             <img src="/E1.jpeg" class="box-border size-32" alt="not found"/>
             <img src="/images/E12.jpeg" class="box-border size-32" alt="not found"/>
