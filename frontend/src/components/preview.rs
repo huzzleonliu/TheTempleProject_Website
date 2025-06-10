b91dd8f0ef4cb3011104a28e27c6a9aff473b4ba
huzz <huzzleonliu@foxmail.com>
2025-06-11 01:39:27 +0200

    SRI问题已基本解决，需要传至服务器确认

--- frontend/src/components/preview.rs
+++ frontend/src/components/preview.rs
@@ -5,8 +5,9 @@ pub fn Preview() -> impl IntoView {
     view! {
         // <div class="text-2xl">
         <div>
-            <img src="http://tp_resource:80/resource/3.jpg" class="box-border size-32" alt="http://tp_resource.:80/3.jpg"/>
-            <img src="http://43.131.27.176:8084/resource/3.jpg" class="box-border size-32" alt="http://43.131.27.176:8084/resource/3.jpg"/>
+            <img src="http://tp_resource.:80/resource/3.jpg" class="box-border size-32" alt="http://tp_resource.:80/3.jpg"/>
+            <img src="http://43.131.27.176:8080/resource/resource/3.jpg" class="box-border size-32" alt="http://43.131.27.176:8080/resource/resource/3.jpg"/>
+            <img src="http://localhost:8080/resource/resource/3.jpg" class="box-border size-32" alt="http://localhost:8080/resource/resource/3.jpg"/>
             <img src="http://localhost:8084/resource/3.jpg" class="box-border size-32" alt="http://localhost:8084/resource/3.jpg"/>
             <img src="/E2.jpg" class="box-border size-32" alt="can not get"/>
             <img src="/E1.jpeg" class="box-border size-32" alt="not found"/>
