59272f0e72492dd58410534e1140bdd2ce1e293f
huzz <huzzleonliu@foxmail.com>
2025-12-19 14:43:17 +0100

    rename after parse

--- utils/review-and-rename/src/logic/rename.rs
+++ utils/review-and-rename/src/logic/rename.rs
@@ -32,6 +32,14 @@ pub fn rename_in_order<E: RenamableEntry>(
 
     let mut final_paths: Vec<(usize, PathBuf, String)> = Vec::with_capacity(entries.len());
     for (idx, entry) in entries.iter().enumerate() {
+        // 获取原始文件名
+        let mut original_name = entry.file_name().to_string();
+        // 如果文件名中包含 _ 且第一个部分是数字，则认为文件已重命名，取第二个部分作为原始文件名
+        let have_renamed = original_name.contains("_") && original_name.split("_").nth(0).is_some_and(|s| s.parse::<usize>().is_ok());
+        if have_renamed {
+            original_name = original_name.split("_").nth(1).unwrap().to_string();
+        }
+        // 获取文件扩展名
         let ext = entry
             .path()
             .extension()
@@ -44,7 +52,7 @@ pub fn rename_in_order<E: RenamableEntry>(
         let new_name = if ext.is_empty() {
             stem.clone()
         } else {
-            format!("{}.{}", stem, ext)
+            format!("{}_{}.{}", stem, original_name, ext)
         };
 
         let final_path = dir.join(&new_name);
